8fa4f1250890e6a9615bde55cbdd82ed511a0b10
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-26 18:01:33 -0400

    q1 done

--- js/Engine.js
+++ js/Engine.js
@@ -57,17 +57,47 @@ class Engine {
     // We check if the player is dead. If he is, we alert the user
     // and return from the method (Why is the return statement important?)
     if (this.isPlayerDead()) {
-      window.alert('Game over');
+      window.alert("Game over");
       return;
     }
 
     // If the player is not dead, then we put a setTimeout to run the gameLoop in 20 milliseconds
     setTimeout(this.gameLoop, 20);
+
+    // check position
+    // console.log(this.player);
+    // for (let i = 0; i < this.enemies.length; i++) {
+    //   console.log(this.enemies[i].x);
+    // }
   };
 
   // This method is not implemented correctly, which is why
   // the burger never dies. In your exercises you will fix this method.
+
   isPlayerDead = () => {
+    for (let i = 0; i < this.enemies.length; i++) {
+      let playerLeft = this.player.x;
+      let playerRight = this.player.x + PLAYER_WIDTH;
+      let playerTop = GAME_HEIGHT - PLAYER_HEIGHT - 10;
+      let playerBot = GAME_HEIGHT - 10;
+      let enemyLeft = this.enemies[i].x;
+      let enemyRight = this.enemies[i].x + ENEMY_WIDTH;
+      let enemyTop = this.enemies[i].y;
+      let enemyBot = this.enemies[i].y + ENEMY_HEIGHT;
+      console.log("playerLeft < enemyRight", playerLeft, enemyRight);
+      console.log("playerRight > enemyLeft", playerRight, enemyLeft);
+      console.log("playerTop < enemyBot", playerTop, enemyBot);
+      console.log("playerBot > enemyTop", playerBot, enemyTop);
+      if (
+        playerLeft < enemyRight &&
+        playerRight > enemyLeft &&
+        playerTop < enemyBot &&
+        playerBot > enemyTop
+      ) {
+        console.log("hit");
+        return true;
+      }
+    }
     return false;
   };
 }
--- js/Player.js
+++ js/Player.js
@@ -15,12 +15,13 @@ class Player {
 
     // We create a DOM node. We will be updating the DOM node every time we move the player, so we store a reference to the
     // DOM node in a property.
-    this.domElement = document.createElement('img');
-    this.domElement.src = 'images/player.png';
-    this.domElement.style.position = 'absolute';
+    this.domElement = document.createElement("img");
+    this.domElement.src = "images/player.png";
+    this.domElement.style.position = "absolute";
     this.domElement.style.left = `${this.x}px`;
     this.domElement.style.top = ` ${y}px`;
-    this.domElement.style.zIndex = '10';
+    this.domElement.style.zIndex = "10";
+    this.domElement.style.border = "3px yellow solid";
     root.appendChild(this.domElement);
   }
 
@@ -42,3 +43,5 @@ class Player {
     this.domElement.style.left = `${this.x}px`;
   }
 }
+
+console.log(Player);
